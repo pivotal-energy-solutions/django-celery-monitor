b707f2ce1390486eb07d309e2e4c0ad92eb089be
Steven Klass <steven@pointcircle.com>
2019-01-21 11:30:23 -0700

    Bump Version and Prep For Localized support

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ class pytest(setuptools.command.test.test):
 
 
 setuptools.setup(
-    name=NAME,
+    name='pivotal_' + NAME,
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     version=meta['version'],
     description=meta['doc'],
